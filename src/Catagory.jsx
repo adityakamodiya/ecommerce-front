2eb7ceaa970cec3b8bbdaac0d8682758680d6e2e
aditiyakamodiya <aditiyakamodiya@gmail.com>
2023-10-06 19:37:27 +0530

    form and catagory page added

---
+++ src/Catagory.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+
+function Catagory() {
+    function Addcatagory(){
+      
+      axios.post('') 
+
+    }
+  return (
+    <div>
+         <form action="" onSubmit={Addcatagory}>
+            <input type="text" />
+            <input type="submit" />
+         </form>
+    </div>
+  )
+}
+
+export default Catagory
